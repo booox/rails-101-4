1b31564071506a37886018258cf8a12ea52be73e
booox <booox@users.noreply.github.com>
2016-12-28 14:34:18 +0800

    modified welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+	def index
+		flash[:notice] = "早安！你好！"
+		flash[:warning] = "just a warning info！"
+		flash[:alert] = "It's time to go to bed！"
+	end
 end
