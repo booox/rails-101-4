784f9bb9fa602d90c3170d2b0132394467442f49
booox <booox@users.noreply.github.com>
2016-12-29 14:36:29 +0800

    add post button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+	before_filter :authenticate_user!, :only => [:new, :create]
+
+	def new
+		@group = Group.find(params[:id])
+		@post = Post.new
+	end
+
+	def create
+		@group = Group.find(params[:id])
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to group_path(@group), notice: "Post Create."
+		else
+			render :new
+		end
+
+	end
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:content)
+	end
 end
